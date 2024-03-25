f8f900d4a2fe360962701b098e4cc17bef1b00e5
djx903109540 <903109540@qq.com>
2024-03-25 19:45:29 +0800

    exer3

---
+++ Ch2_7.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Ch2_20240222;
+
+/**
+ *
+ * @author 蓮
+ */
+public class Ch2_7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i = 1; i <= 5; i++){
+            for(int k = i; k <= 5; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
