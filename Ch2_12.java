cde4a9c6915874588c2dcd3bfa9c718dd3e118e5
djx903109540 <903109540@qq.com>
2024-03-26 19:20:29 +0800

    exer4

---
+++ Ch2_12.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Ch2_20240222;
+
+/**
+ *
+ * @author 蓮
+ */
+public class Ch2_12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] scoreArray = new int[5];
+        scoreArray[1] = 92;
+        scoreArray[3] = 81;
+        for(int i = 0; i < scoreArray.length; i++){
+            System.out.print(scoreArray[i] + " ");
+        }
+    }
+    
+}
