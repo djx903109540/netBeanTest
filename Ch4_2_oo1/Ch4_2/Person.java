e1db75687bd086b7e6212f941866b2467f8f0997
djx903109540 <903109540@qq.com>
2024-04-03 22:48:32 +0800

    Lesson4-2

---
+++ Ch4_2_oo1/Ch4_2/Person.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ch4_20240229.Ch4_2_oo1;
+
+/**
+ *
+ * @author 蓮
+ */
+public class Person {
+    
+    //屬性
+    String name;    //姓名
+    int age;        //年齡
+    
+    Person(){
+        
+    }
+    
+    Person(String n, int a){
+        this.name = n;
+        this.age = a;
+    }
+    
+    
+}
